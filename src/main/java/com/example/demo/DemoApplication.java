f0e981d4d9692828e2b9eb093e950b1af29cc26c
vduppalapudi <Veerraju.Duppalapudi01@amwater.com>
2022-11-08 21:50:51 +0530

    Example 1st commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,13 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
-	public static String  welcomeMessage="welcome to aws application ... commit test one two";
+	public static String  welcomeMessage="welcome to aws application ... commit test one two 2nd";
 	@GetMapping("/")
 	public String getMessage() {
 		return welcomeMessage;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	public void gitExamples() {
+		// 1 commit 
 	}
 
 }
